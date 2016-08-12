fa692e3135a964774d7e020f0216cdadd57cb8a5
hegdeashwin <ashwin.hegde3@gmail.com>
2016-08-12 23:18:42 +0530

    Re-factors menu component

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -14,6 +14,36 @@ let template = [{
     role: 'close'
   }]
 }, {
+  label: 'Edit',
+  submenu: [{
+    label: 'Undo',
+    accelerator: 'CmdOrCtrl+Z',
+    role: 'undo'
+  }, {
+    type: 'separator'
+  }, {
+    label: 'Cut',
+    accelerator: 'CmdOrCtrl+X',
+    role: 'cut'
+  }, {
+    label: 'Copy',
+    accelerator: 'CmdOrCtrl+C',
+    role: 'copy'
+  }, {
+    label: 'Paste',
+    accelerator: 'CmdOrCtrl+V',
+    role: 'paste'
+  }, {
+    type: 'separator'
+  }, {
+    label: 'Delete',
+    role: 'delete'
+  }, {
+    label: 'Select All',
+    accelerator: 'CmdOrCtrl+A',
+    role: 'selectall'
+  }]
+}, {
   label: 'Developers',
   submenu: [{
     label: 'Release Page',
@@ -37,6 +67,12 @@ let template = [{
     click: function() {
       electron.shell.openExternal('https://github.com/Frontcore/frontcore-desktop/pulls');
     }
+  }]
+}, {
+  label: 'window',
+  submenu: [{
+    label: 'Minimize',
+    role: 'minimize'
   }]
 }, {
   label: 'Help',
